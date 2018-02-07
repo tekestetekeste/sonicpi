6ee93abf479b6d02d2d1ffd985ca0445db22a72a
tekestetekeste <kastekeste@gmail.com>
2018-02-07 14:08:01 -0800

    Add files via upload

---
+++ strangr things.rb
@@ -0,0 +1,26 @@
+# Stranger Things Main Theme
+use_bpm 160
+use_synth :saw
+
+#first set of eighth notes
+play :c2
+sleep 0.5
+play :e2
+sleep 0.5
+play :g2
+sleep 0.5
+play :b2
+sleep 0.5
+
+# second set of eigth notes
+live_loop:strange do
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  
+end
